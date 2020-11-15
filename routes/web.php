2d5e308452bf9dc92bbed9981e23c827e1c9b176
smoeller02-intra <sebastian@smoeller-web.de>
2020-11-15 11:06:23 +0100

    Login eingebaut

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
-Route::get('/test', function () {
-    return view('test');
-});
+
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
